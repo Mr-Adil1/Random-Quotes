d139f15c97a135581a503b0e5dc8536b7d0eb89d
Muhammad Adil <mr.web.developer3@gmsil.com>
2022-04-16 14:07:24 -0700

    speech stope

--- min.js
+++ min.js
@@ -4,12 +4,15 @@ const quot_area = document.querySelector('.quot-area');
 const quotBtn = document.querySelector('button');
 const authername = document.querySelector('.name');
 const soundbtn = document.querySelector('.sound');
+const volumicon = document.getElementById('sound');
 const copy = document.querySelector('.copy');
 const twittwr = document.querySelector('.twitter');
 const copied = document.querySelector('.copied')
 
 // random_Quote_function
 quotBtn.addEventListener('click', () => {
+    soundbtn.classList.remove('blurr');
+    speechSynthesis.cancel();
     quotBtn.classList.add('loading');
     quotBtn.innerHTML = 'Loading...'
     quotetext.innerHTML = '<span class="spinner-border spinner-border-sm"></span> Loading Data..'
@@ -26,7 +29,11 @@ quotBtn.addEventListener('click', () => {
 });
 
 soundbtn.addEventListener('click', () => {
+    soundbtn.classList.add('blurr');
     let uttrence = new SpeechSynthesisUtterance(`${quotetext.innerHTML} by ${authername.innerHTML}`);
+    uttrence.addEventListener('end', () => {
+        soundbtn.classList.remove('blurr');
+    })
     speechSynthesis.speak(uttrence);
 })
 
